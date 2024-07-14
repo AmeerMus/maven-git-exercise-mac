de1b8c3451f2d08093308b137eeb7ff557467880
AmeerMus <ameermus17@gmail.com>
2024-07-14 22:41:16 +0800

    test

--- src/main/java/com/sddevops/maven/eclipse/Restaurant.java
+++ src/main/java/com/sddevops/maven/eclipse/Restaurant.java
@@ -4,9 +4,5 @@ public class Restaurant {
 	private int id;
 	private String name;
 	private String address;
-<<<<<<< HEAD
-	private int number;
-=======
->>>>>>> branch 'master' of https://github.com/AmeerMus/maven-git-exercise-mac.git
 
 }
